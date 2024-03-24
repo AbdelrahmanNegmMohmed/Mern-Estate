2c2ed84bd12ce6792b310e26831af77c28862bcf
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-03-24 23:34:40 +0200

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import Profille from "./Pages/Profille";
 export default function App() {
   return (
-    <div className='text-red-100'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profille />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/Pages/Profille.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Profille() {
+  return (
+    <div>Profille</div>
+  )
+}
