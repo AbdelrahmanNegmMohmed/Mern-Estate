d2595714c12a9eb0a3dec832bee8fafac6e01de6
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-03-29 13:55:00 +0200

    create search api route

--- api/routes/listing.Route.js
+++ api/routes/listing.Route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { veryToken } from '../utils/VeryFiyUser.js';
-import { createListing,deleteListing,updateListing,getListing } from '../controller/listing.controller.js';
+import { createListing,deleteListing,updateListing,getListing,getListings } from '../controller/listing.controller.js';
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.post('/create',veryToken, createListing);
 router.delete('/delete/:id',veryToken,deleteListing);
 router.post('/update/:id', veryToken, updateListing);
 router.get('/get/:id',getListing);
-
+router.get('/get',getListings)
 
 export default router;
