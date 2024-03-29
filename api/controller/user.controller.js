6d8e7ca873c2ffb8bb6fff456dfb8daf32bff340
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-03-29 11:09:03 +0200

    Add contact landloder functionality to the listing page

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -72,3 +72,18 @@ export const getUserListings =async (request, response, next)  =>{
     return next (errorHandelar(401,"You can only view your own listings"))
   }
   }
+
+
+  export const getUser =async(req,res,next)=>{
+
+    try {
+      const user =await User.findById(req.params.id);
+      if (!user) return next(errorHandelar(404,"User not found !"));
+      const {password:pass , ...rest}=user._doc;
+      res.status(200).json(rest);
+      
+    } catch (error) {
+      next(error)
+    }
+
+  }
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser,deletUser,getUserListings} from '../controller/user.controller.js';
+import { test, updateUser,deletUser,getUserListings,getUser} from '../controller/user.controller.js';
 import { veryToken } from '../utils/VeryFiyUser.js';
 
 
@@ -7,6 +7,7 @@ const router =express.Router();
 router.get ('/test', test );
 router.post('/update/:id',veryToken,updateUser);
 router.delete('/delete/:id',veryToken,deletUser);
-router.get('/listings/:id',veryToken,getUserListings)
+router.get('/listings/:id',veryToken,getUserListings);
+router.get('/:id',veryToken,getUser)
 
 export default router;
