0082484b40e7e8f43b3e4c87faf41d0f315ec3b6
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-03-29 05:59:21 +0200

    create update listing api route

--- api/routes/listing.Route.js
+++ api/routes/listing.Route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { veryToken } from '../utils/VeryFiyUser.js';
-import { createListing,deleteListing } from '../controller/listing.controller.js';
+import { createListing,deleteListing,updateListing } from '../controller/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create',veryToken, createListing);
-router.delete('/delete/:id',veryToken,deleteListing)
+router.delete('/delete/:id',veryToken,deleteListing);
+router.post('/update/:id', veryToken, updateListing);
 
 export default router;
